40301186e09cbecf6ae690ce307697cb7de3974d
Nuraiym0 <nuraiymmamytova0@gmail.com>
2022-11-30 14:54:19 +0600

    shop commit

--- main/models.py
+++ main/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-class Student(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    age = models.DecimalField(max_digits=100, decimal_places=2)
-    qrade = models.IntegerField()
-    
+# Create your models here.
 
-
-
-# 2. создайте django проект с приложением school создайте модель student с полями first_name, last_name, age, grade
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    description = models.TextField()

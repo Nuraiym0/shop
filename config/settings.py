70e4c2f8d47b75284348847fb2bc9f48a0139f65
Nuraiym0 <nuraiymmamytova0@gmail.com>
2022-11-30 14:46:48 +0600

    school commit

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'django_shop',
+        'NAME': 'django_school',
         'USER': 'nuraiym',
         'PASSWORD' : '1',
         'HOST':'127.0.0.1',
--- main/models.py
+++ main/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.DecimalField(max_digits=100, decimal_places=2)
+    qrade = models.IntegerField()
+    
 
-class Product(models.Model):
-    title = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField()
-    description = models.TextField()
+
+
+# 2. создайте django проект с приложением school создайте модель student с полями first_name, last_name, age, grade
